5f95c8a31c0b1f0d2c031fc839fc3307ebc115b5
Nícolas <nicolas_cioccio@estudante.sc.senai.br>
2024-03-06 15:34:08 -0300

    Soma de arrays no backend

---
+++ teste.js
@@ -0,0 +1,17 @@
+const readlineSync = require('readline-sync');
+
+let array1 = [1, 2, 3, 4, 5];
+let array2 = [];
+let array3 = [];
+
+
+function ler() {
+
+}
+
+function subtracao() {
+    
+}
+
+ler();
+console.log("A subtração entre os dois arrays é = ", subtracao());
